39f15a267696fe8d7c80d1ec6f8eacafd3233f79
Andrew McDonough <andrew@andrewmcdonough.co.uk>
2011-05-03 22:13:12 +0100

    renumber hole

--- spec/golf_spec.rb
+++ spec/golf_spec.rb
@@ -11,18 +11,17 @@ describe "Golf" do
   end
   describe ".hole6" do
     it "should play fizzbuzz to 3, where multiples of 3 are 'fizz'" do
-      Golf.hole2(3).should == [1,2,"fizz"]
+      Golf.hole6(3).should == [1,2,"fizz"]
     end
     it "should play fizzbuzz to 5, with rules above and multiples of 5 are 'buzz'" do
-      Golf.hole2(5).should == [1,2,"fizz",4,"buzz"]
+      Golf.hole6(5).should == [1,2,"fizz",4,"buzz"]
     end
     it "should play fizzbuzz to 10 with rules above and muliples of 3 and 5 are 'fizzbuzz'" do
-      Golf.hole2(15).should == [1,2,"fizz",4,"buzz","fizz",7,8,"fizz","buzz",11,"fizz",13,14,"fizzbuzz"]
+      Golf.hole6(15).should == [1,2,"fizz",4,"buzz","fizz",7,8,"fizz","buzz",11,"fizz",13,14,"fizzbuzz"]
     end
     it "should play fizzbuzz to 30" do
-      Golf.hole2(30).should == [1,2,"fizz",4,"buzz","fizz",7,8,"fizz","buzz",11,"fizz",13,14,"fizzbuzz",16,17,"fizz",19,"buzz","fizz",22,23,"fizz","buzz",26,"fizz",28,29,"fizzbuzz"]
+      Golf.hole6(30).should == [1,2,"fizz",4,"buzz","fizz",7,8,"fizz","buzz",11,"fizz",13,14,"fizzbuzz",16,17,"fizz",19,"buzz","fizz",22,23,"fizz","buzz",26,"fizz",28,29,"fizzbuzz"]
     end
-
   end
 end
 
