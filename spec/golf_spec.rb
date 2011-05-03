f0cc75cff0b3ecf74936fd79d4d70b24c8e40c06
Andrew McDonough <andrew@andrewmcdonough.co.uk>
2011-05-03 23:48:00 +0100

    Fibonacci hole

--- spec/golf_spec.rb
+++ spec/golf_spec.rb
@@ -1,6 +1,7 @@
 require File.dirname(__FILE__) + "/spec_helper"
 
 describe "Golf" do
+
   describe ".hole1" do
     it "should multiply the numbers in an array" do
       Golf.hole1([1,2,3,4]).should == 24
@@ -9,6 +10,7 @@ describe "Golf" do
       Golf.hole1([5,2,10,6]).should == 600
     end
   end
+
   describe ".hole6" do
     it "should play fizzbuzz to 3, where multiples of 3 are 'fizz'" do
       Golf.hole6(3).should == [1,2,"fizz"]
@@ -23,5 +25,18 @@ describe "Golf" do
       Golf.hole6(30).should == [1,2,"fizz",4,"buzz","fizz",7,8,"fizz","buzz",11,"fizz",13,14,"fizzbuzz",16,17,"fizz",19,"buzz","fizz",22,23,"fizz","buzz",26,"fizz",28,29,"fizzbuzz"]
     end
   end
+
+  describe ".hole8" do
+    it "should give the first N numbers of the fibonacci sequence" do
+      Golf.hole8(5).should == [1,1,2,3,5]
+    end
+    it "should give the first 9 numbers" do
+      Golf.hole8(9).should == [1,1,2,3,5,8,13,21,34]
+    end
+    it "should give the first 15 numbers" do
+      Golf.hole8(15).should == [1,1,2,3,5,8,13,21,34,55,89,144,233,377,610]
+    end
+  end
+
 end
 
