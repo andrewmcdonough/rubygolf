0e020f55e14bd21c50910b066793e9276e3a9f79
Simon Coffey <simon@tribesports.com>
2011-05-09 16:06:40 +0100

    Cats, dogs and sub-lists

--- spec/golf_spec.rb
+++ spec/golf_spec.rb
@@ -6,16 +6,17 @@ describe "Golf" do
     it "should multiply the numbers in an array" do
       Golf.hole1([1,2,3,4]).should eql 24
     end
+
     it "should be 600 for [5,2,10,6]" do
       Golf.hole1([5,2,10,6]).should eql 600
     end
   end
 
+
   describe ".hole2" do
     it "should sort a sentence by the second letter of each word" do
       Golf.hole2("the quick brown fox").should eql "the fox brown quick"
     end
-
   end
 
 
@@ -26,43 +27,87 @@ describe "Golf" do
   end
 
 
+  describe ".hole4" do
+    it "should put a hat on every man" do
+      Golf.hole4(["man(bob)", "man(geoff)", "man(harry)"]).should ==
+        ["hat(man(bob))", "hat(man(geoff))", "hat(man(harry))"]
+    end
+
+    it "should feed each dog a bone" do
+      Golf.hole4(["dog(luna)", "dog(rex)", "dog(spot)"]).should ==
+        ["dog(luna(bone))", "dog(rex(bone))", "dog(spot(bone))"]
+    end
+
+    it "should collapse the probability waveform of each cat" do
+      Golf.hole4(["cat(tiddles)", "cat(miffy)"]).should ==
+        ["dead(tiddles)", "dead(miffy)"]
+    end
+
+    it "should do all the above at once" do
+      Golf.hole4(["man(steve)", "dog(snoopy)", "cat(garfield)"]).should ==
+        ["hat(man(steve))", "dog(snoopy(bone))", "dead(garfield)"]
+    end
+  end
+
+
+  describe ".hole5" do
+    it "should return all sub-lists of the input, sorted by length then numerically" do
+      Golf.hole5([1,2,3,4]).should ==
+        [[1], [2], [3], [4], [1, 2], [2, 3], [3, 4], [1, 2, 3], [2, 3, 4], [1, 2, 3, 4]]
+
+      Golf.hole5([4,10,15,23]).should ==
+        [[4], [10], [15], [23], [4, 10], [10, 15], [15, 23], [4, 10, 15], [10, 15, 23], [4, 10, 15, 23]]
+    end
+  end
+
+
   describe ".hole6" do
     it "should play fizzbuzz to 3, where multiples of 3 are 'fizz'" do
       Golf.hole6(3).should eql [1,2,"fizz"]
     end
+
     it "should play fizzbuzz to 5, with rules above and multiples of 5 are 'buzz'" do
       Golf.hole6(5).should eql [1,2,"fizz",4,"buzz"]
     end
+
     it "should play fizzbuzz to 10 with rules above and muliples of 3 and 5 are 'fizzbuzz'" do
       Golf.hole6(15).should eql [1,2,"fizz",4,"buzz","fizz",7,8,"fizz","buzz",11,"fizz",13,14,"fizzbuzz"]
     end
+
     it "should play fizzbuzz to 30" do
       Golf.hole6(30).should eql [1,2,"fizz",4,"buzz","fizz",7,8,"fizz","buzz",11,"fizz",13,14,"fizzbuzz",16,17,"fizz",19,"buzz","fizz",22,23,"fizz","buzz",26,"fizz",28,29,"fizzbuzz"]
     end
   end
 
+
   describe ".hole8" do
     it "should give the first N numbers of the fibonacci sequence" do
       Golf.hole8(5).should eql [1,1,2,3,5]
     end
+
     it "should give the first 9 numbers" do
       Golf.hole8(9).should eql [1,1,2,3,5,8,13,21,34]
     end
+
     it "should give the first 15 numbers" do
       Golf.hole8(15).should eql [1,1,2,3,5,8,13,21,34,55,89,144,233,377,610]
     end
   end
 
+
   describe ".hole9" do
     it "should return the winner of an AV election where winner has a first round majority" do
       Golf.hole9('fixtures/av-example1.txt').should eql "blue"
     end
+
     it "should eliminate the last candidate if there is no first round majority and allocate their second choices" do
       Golf.hole9('fixtures/av-example2.txt').should eql "red"
     end
+
     it "should continue to eliminate the losing candidate until a majority is reached" do
       Golf.hole9('fixtures/av-example3.txt').should eql "blue"
     end
+
     it "should be the candidate with most votes if there are only two left, even witout a majority" do
       Golf.hole9('fixtures/av-example4.txt').should eql "blue"
     end
